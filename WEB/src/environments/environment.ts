012032a07a685410232660b524d84d5b78168983
the97prime <oscar.penaherrera@epn.edu.ec>
2020-02-11 17:17:28 -0500

    Aplicativo de Escritorio App Reservas

---
+++ WEB/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDfMDzhVpqmY7Pa_rzppxUvA3i-bCvhW6s",
+    authDomain: "soa-aplicacion.firebaseapp.com",
+    databaseURL: "https://soa-aplicacion.firebaseio.com",
+    projectId: "soa-aplicacion",
+    storageBucket: "soa-aplicacion.appspot.com",
+    messagingSenderId: "261718882776",
+    appId: "1:261718882776:web:a699b60dab0daeb4585aad"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
