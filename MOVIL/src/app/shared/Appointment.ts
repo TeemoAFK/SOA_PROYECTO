75a325e80c81f57ea4aa840f98646b05c598aba8
the97prime <oscar.penaherrera@epn.edu.ec>
2020-02-11 17:52:01 -0500

    App Reservas mobil Android

---
+++ MOVIL/src/app/shared/Appointment.ts
@@ -0,0 +1,9 @@
+export class Appointment {
+    $key: string;
+    name: string;
+    other: string;
+    checkin: string;
+    checkout: string;
+    email: string;
+    mobile: number;
+}
---
+++ MOVIL/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDfMDzhVpqmY7Pa_rzppxUvA3i-bCvhW6s",
+    authDomain: "soa-aplicacion.firebaseapp.com",
+    databaseURL: "https://soa-aplicacion.firebaseio.com",
+    projectId: "soa-aplicacion",
+    storageBucket: "soa-aplicacion.appspot.com",
+    messagingSenderId: "261718882776",
+    appId: "1:261718882776:web:a699b60dab0daeb4585aad"
+  }
+};
